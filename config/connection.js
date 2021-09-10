fde331e32417e08752454b55cb1e7ed08ccd20fe
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-09 21:21:39 -0400

    reviewed connection.js file in config..

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
+// sql db credential import
 require('dotenv').config();
 
+//https://sequelize.org/ - a promise-based Node.js ORM for Postgres, MySQL, MariaDB, SQLite and Microsoft SQL Server.
 const Sequelize = require('sequelize');
 
+//https://elements.heroku.com/addons/jawsdb incase the app is deployed on heroku 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -11,5 +14,5 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
-
+// exporting sequelize for future use
 module.exports = sequelize;
