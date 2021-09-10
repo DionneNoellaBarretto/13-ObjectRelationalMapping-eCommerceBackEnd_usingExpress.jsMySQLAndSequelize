2db6b2f618cc81c6e989a82dc5cae4a939b1d451
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-09 21:18:01 -0400

    double checked set up in server.js and added contextual comment

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+//importing dependencies: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 
+// This app starts a server and listens on port 3001 for connections
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//using middleware https://expressjs.com/en/guide/using-middleware.html
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// route usage deifned https://expressjs.com/en/4x/api.html#app.use
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
