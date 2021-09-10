9fdf3d665676b51cae3b8dd970c0dc62511b5814
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-09 22:24:31 -0400

    asyn await syntax correction

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
   });
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 
@@ -41,7 +41,7 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
 		const categoryData = await Category.create(req.body);
@@ -51,7 +51,7 @@ router.post('/', (req, res) => {
 	}
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update(req.body, {
@@ -66,7 +66,7 @@ router.put('/:id', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
